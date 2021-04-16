bd4a543be7b28c0a80dfd60e81cd0c53b037909a
iury-araujo <iury.araujo1210@gmail.com>
2021-04-16 12:57:59 -0300

    Finalizado

--- urna/etapas.js
+++ urna/etapas.js
@@ -1,23 +1,23 @@
-let etapas = [
-    {
+let etapas = [{
         titulo: 'VEREADOR',
         numeros: 5,
-        candidatos: [
-            {
-                numero: 38111,
+        candidatos: [{
+                numero: '38111',
                 nome: 'Fulano de tal',
                 partido: 'ABC',
-                fotos: [
-                    { url: '38111.jpg', legenda: 'Vereador' }
-                ]
+                fotos: [{
+                    url: '38111.jpg',
+                    legenda: 'Vereador'
+                }]
             },
             {
-                numero: 77222,
+                numero: '77222',
                 nome: 'Beltrano da Silva',
                 partido: 'DEFG',
-                fotos: [
-                    { url: '77222.jpg', legenda: 'Vereador' }
-                ]
+                fotos: [{
+                    url: '77222.jpg',
+                    legenda: 'Vereador'
+                }]
             },
         ]
 
@@ -25,27 +25,39 @@ let etapas = [
     {
         titulo: 'PREFEITO',
         numeros: 2,
-        candidatos: [
-            {
-                numero: 99,
+        candidatos: [{
+                numero: '99',
                 nome: 'Ciclano',
                 partido: 'ABC',
                 vice: 'Cic',
-                fotos: [
-                    { url: '99.jpg', legenda: 'Prefeito' },
-                    { url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                fotos: [{
+                        url: '99.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '99_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
                 ]
             },
             {
-                numero: 84,
+                numero: '84',
                 nome: 'Zulano',
                 partido: 'QWERTY',
                 vice: 'Zul',
-                fotos: [
-                    { url: '84.jpg', legenda: 'Prefeito' },
-                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                fotos: [{
+                        url: '84.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '84_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
                 ]
             },
         ]
     }
 ];
+;
